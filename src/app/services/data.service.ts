2e5ff16dcfc0c1790b4405b963ad290a16da224c
John Paul Gingpis <65699867+devjaypeee@users.noreply.github.com>
2024-03-03 17:19:31 +0800

    Update data.service.ts

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -8,7 +8,7 @@ export class DataService {
 
   constructor(private http: HttpClient) { }
 
-  private apiUrl = 'https://vercel.com/john-paul-gingpis-projects/praise-api';
+  private apiUrl = 'https://srv544-files.hstgr.io/48fbb937866a3a34/files/public_html/raise-api/';
 
   apiRequest(method: string, data: any) {
     return this.http.post(this.apiUrl + method, data);
